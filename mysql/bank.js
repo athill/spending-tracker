1ebe9afe8e0e21ea45e2f2f786a6305a375f3650
Andy Hill <andy@andyhill.us>
2021-11-18 21:02:39 -0500

    add bank record processor

---
+++ mysql/bank.js
@@ -0,0 +1,77 @@
+const parse  = require('csv-parse/lib/sync');
+const fs = require('fs');
+
+const MySqlService = require('./MySqlService');
+const Transaction = require('../backend/models/Transaction');
+
+// input validation
+const usage = 'Usage: node bank.js <csv-file>';
+if (process.argv.length != 3) {
+    console.log(usage);
+    process.exit(1);
+}
+
+// let's roll
+
+// parse csv
+const csvFile = process.argv[2];
+const data = fs.readFileSync(csvFile).toString();
+
+const records = parse(data, {
+    columns: true,
+    skip_empty_lines: true,
+    from_line: 4,
+});
+
+
+// helpers
+const inMemo = (record, regex) => regex.test(record.Memo);
+
+const inDescription = (record, regex) => regex.test(record.Description);
+
+const bills = [
+    {
+        test: (record) => inMemo(record, /^NETFLIX\.COM/),
+        category: 'entertainment',
+        item: 'Netflix',
+        store: 'Netflix'
+    },
+    {
+        test: (record) => inMemo(record, /^CITY OF BLOOMIN /),
+        category: 'utilities',
+        item: 'water',
+        store: 'CBU'
+    },
+    {
+        test: (record) => inDescription(record, /^AUTO-OWNERS INS. PREM/),
+        category: 'insurance',
+        item: 'insurance',
+        store: 'Auto-Owners insurance'
+    },
+    {
+        test: (record) => inMemo(record, /^DIGITALOCEAN/),
+        category: 'web',
+        item: 'digitalocean',
+        store: 'DigitalOcean'
+    },
+];
+
+const main = () => {
+    records.forEach(async record => {
+        bills.forEach(async ({ category, item, store, test }) => {
+            if (test(record)) {
+                const transaction = Transaction.of({
+                    category,
+                    date: record.Date,
+                    item,
+                    store,
+                    price: -parseFloat(record['Amount Debit'])
+                });
+                console.log(transaction);
+                await  transaction.store();
+            }
+        });
+    });
+};
+
+main();
